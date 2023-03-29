023b2b7833b4c04c0ef3e3f7549c382ce48d4d14
pwnjack <pwnjack@gmail.com>
2023-03-30 01:53:59 +0200

    index fix

--- hello-doom.sh
+++ hello-doom.sh
@@ -6,14 +6,14 @@ mapfile -t lines < doom_lines.txt
 # Get the length of the array
 length=${#lines[@]}
 
-# Set the random seed for improved performance
-RANDOM_SEED=$(shuf -i 0-32767 -n 1)
-
 # Infinite loop to output random doom metal line every 5-10 minutes
 while true
 do
+  # Set the random seed for improved performance
+  RANDOM_SEED=$(shuf -i 0-32767 -n 1)
+
   # Generate a random index
-  index=$((RANDOM_SEED % $((length-1))))
+  index=$((RANDOM_SEED % length))
 
   # Output the line
   printf '%s\n' "${lines[index]}"
