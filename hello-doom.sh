7008ff604c0bfef6d07499e2c5413cf4e9160d94
pwnjack <pwnjack@gmail.com>
2023-03-30 01:42:10 +0200

    optimize

--- hello-doom.sh
+++ hello-doom.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Read the lines from the file into an array
-readarray -t lines < doom_lines.txt
+mapfile -t lines < doom_lines.txt
 
 # Get the length of the array
 length=${#lines[@]}
 
 # Set the random seed for improved performance
-RANDOM=$$$(date +%s)
+RANDOM_SEED=$(shuf -i 0-32767 -n 1)
 
 # Infinite loop to output random doom metal line every 5-10 minutes
 while true
 do
   # Generate a random index
-  index=$((RANDOM % length))
-  
+  index=$((RANDOM_SEED % $((length-1))))
+
   # Output the line
   printf '%s\n' "${lines[index]}"
-  
+
   # Sleep for a random duration between 5-10 minutes
-  sleep $((5 + RANDOM % 6))m
+  sleep $((RANDOM_SEED % 6 + 5))m
 done
